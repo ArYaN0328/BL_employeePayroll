a9149f83a9a78889648f85797bd73bad6b667d9f
ArYaN0328 <121503458+ArYaN0328@users.noreply.github.com>
2025-03-06 17:03:02 +0530

    Controller made

---
+++ src/main/java/com/example/employeepayroll/Controller/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.example.employeepayroll.Controller;
+
+
+import com.example.employeepayroll.Service.EmployeeService;
+import com.example.employeepayroll.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+//UC2
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+
+    @Autowired
+    EmployeeService empservice;
+
+    @GetMapping
+    public List<Employee> getall()
+    {
+        return empservice.getAll();
+    }
+    @GetMapping("/{id}")
+    public Employee getbyid(@PathVariable Long id)
+    {
+        return empservice.getbyId(id);
+    }
+    @PostMapping
+    public Employee posting(@RequestBody Employee emp)
+    {
+       return empservice.addEmployee(emp);
+
+    }
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return empservice.updateEmployee(id, employee);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        empservice.deleteByid(id);
+        return "Employee Deleted Successfully";
+    }
+
+
+
+}
